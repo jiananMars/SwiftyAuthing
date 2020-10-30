729247947d275c12332cb106525c6303bcf3ae06
Eric <aloveric@gmail.com>
2020-10-30 20:17:54 +0800

    feat: update package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyAuthing",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,18 +17,21 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "0.36.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.3.0"),
-        .package(url: "https://github.com/Woo-Interactive/SwiftyRSA.git", from: "1.7.1"),
+        .package(url: "https://gitee.com/aloveric/apollo-ios.git", from: "0.36.0"),
+        .package(url: "https://gitee.com/aloveric/Alamofire.git", from: "5.3.0"),
+        .package(url: "https://gitee.com/aloveric/SwiftyRSA.git", from: "1.7.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyAuthing",
-            dependencies: []),
+            dependencies: ["Apollo", "Alamofire", "SwiftyRSA"]),
         .testTarget(
             name: "SwiftyAuthingTests",
             dependencies: ["SwiftyAuthing"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
--- SwiftyAuthingExample/SwiftyAuthingExample/ViewController.swift
+++ SwiftyAuthingExample/SwiftyAuthingExample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftyAuthing
 
 class ViewController: UIViewController {
 
