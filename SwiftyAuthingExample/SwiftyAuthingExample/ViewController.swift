19d1ddf5c612c542b0b0b93895fe4859a45cc706
Eric <aloveric@gmail.com>
2020-10-30 18:03:14 +0800

    feat: add example

---
+++ SwiftyAuthingExample/SwiftyAuthingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyAuthingExample
+//
+//  Created by Eric Woo on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
